3670e766c12953e890783dde1972e48d423fe02a
Harloom <ilhamsolehudin1999@gmail.com>
2019-04-08 16:30:47 +0700

    Chart Post Delete

--- Objects/Charts.js
+++ Objects/Charts.js
@@ -28,7 +28,7 @@ const getChart = async (id_ktp, keyAPI, callback) => {
         .execute('getChart')
       await callback(result2);
       await mssql.close();
-    }else{
+    } else {
       callback(false);
     }
 
@@ -39,11 +39,48 @@ const getChart = async (id_ktp, keyAPI, callback) => {
 
   }
 
-  const saveChart = async ()=>{
-    
-  }
-
-
 
 }
+
+const saveChart = async (Chart, _key, callback) => {
+  try {
+
+    let valid = await getValidKeyAPI(_key);
+
+    if (valid) {
+
+      let pool = await getPool();
+      let result = await pool.request()
+        .input('kode_ktp', mssql.Char(16), Chart.id_ktp)
+        .input('id_kamera', mssql.Char(5), Chart.id_kamera)
+        .input('jumlah_pinjam', mssql.Int, parseInt(Chart.jumlah))
+        .execute('save_chart');
+      
+      callback(result.recordset);
+    }
+  } catch (error) {
+    callback(false)
+  }
+
+}
+
+const delete_chart = async (Chart, _key, callback) => {
+  try {
+    let valid = await getValidKeyAPI(_key);
+    if (valid) {
+
+      let pool = await getPool();
+      let result = await pool.request()
+        .input('kode_ktp', mssql.Char(16), Chart.id_ktp)
+        .input('id_kamera', mssql.Char(5), Chart.id_kamera)
+        .execute('delete_chart_product');
+      console.log(result);
+      callback(result.recordset);
+    }
+  } catch (error) {
+    callback(false);
+  }
+}
 module.exports.getChart = getChart;
+module.exports.saveChart = saveChart;
+module.exports.deleteChart = delete_chart;
--- main_api/chartRouters.js
+++ main_api/chartRouters.js
@@ -34,11 +34,41 @@ router.put('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  
+  let valid = Joi.validate(req.body,SchemaPost,{escapeHtml: true});
+  if(valid.error){
+    res.status(400).send({massage : "404 Bad Request Body"});
+  }else{
+    const validateHeader = Joi.validate(req.headers.key_api,SchemaHeaders,{escapeHtml: true});
+    if(validateHeader.error){
+      res.status(400).send({massage : "404 Bad Request header"});
+    }else{
+        Chart.saveChart(valid.value,validateHeader.value,(result)=>{
+            !result?
+              res.status(403).send({massage : "Products Sudah Ada"}) : res.status(200).send(result);
+    
+        });
+    }
+  }
+
+
 });
 
 router.delete('/', (req, res) => {
-  
+  let valid = Joi.validate(req.body,SchemaDelete,{escapeHtml: true});
+  if(valid.error){
+    res.status(400).send({massage : "404 Bad Request Body"});
+  }else{
+    const validateHeader = Joi.validate(req.headers.key_api,SchemaHeaders,{escapeHtml: true});
+    if(validateHeader.error){
+      res.status(400).send({massage : "404 Bad Request header"});
+    }else{
+        Chart.deleteChart(valid.value,validateHeader.value,(result)=>{
+            !result?
+              res.status(403).send({massage : "Products Tidak Ada"}) : res.status(200).send(result);
+    
+        });
+    }
+  }
 });
 
 
@@ -48,4 +78,13 @@ const Schema ={
 }
 const SchemaHeaders = Joi.string().min(1).required();
 
+const SchemaPost = {
+  id_ktp : Joi.string().min(16).max(16).required(),
+  id_kamera : Joi.string().min(5).max(5).required(),
+  jumlah : Joi.string().min(1).max(3).required()
+}
+const SchemaDelete  ={
+  id_ktp : Joi.string().min(16).max(16).required(),
+  id_kamera : Joi.string().min(5).max(5).required()
+}
 module.exports = router;
